43a18ad4eb9d29e2f061dd4681705424b7436a3a
Austin Ouyang <aouyang1@gmail.com>
2020-05-31 14:22:21 -0700

    changed to use Report struct

--- cmd/main.go
+++ cmd/main.go
@@ -18,45 +18,86 @@ var (
 	timeLayout = "2006-01-02T15:04:05Z"
 )
 
-type LineItemID struct {
-	UID      uint64
-	Start    time.Time
-	End      time.Time
-	Bill     *Bill
-	LineItem *LineItem
+type Report struct {
+	LineItems map[time.Time][]*LineItem // map of start timestamps to a slice of LineItemIDs
+	TimePts   []time.Time               // sorted order of start timestamps with identity
 }
 
-func NewLineItemID(id, timeInterval string) (*LineItemID, error) {
-	l := new(LineItemID)
-	l.UID = xxhash.Sum64String(id)
-	timeIntStr := strings.Split(timeInterval, "/")
-	if len(timeIntStr) != 2 {
-		return nil, fmt.Errorf("Invalid time interval, %s", timeInterval)
-	}
-
+func NewReport(filename string) (*Report, error) {
 	var err error
-	l.Start, err = time.Parse(timeLayout, timeIntStr[0])
+
+	r := &Report{LineItems: make(map[time.Time][]*LineItem)}
+	fh, err := os.Open(filename)
 	if err != nil {
-		return nil, fmt.Errorf("Could not parse start interval, %v", err)
+		return nil, err
 	}
-	l.End, err = time.Parse(timeLayout, timeIntStr[1])
+
+	gz, err := gzip.NewReader(fh)
 	if err != nil {
-		return nil, fmt.Errorf("Coult not parse end interval, %v", err)
+		return nil, err
 	}
-	return l, nil
+
+	scanner := bufio.NewScanner(gz)
+
+	scanner.Scan()
+	headers := strings.Split(scanner.Text(), ",")
+	headerIdx := make(map[string]int)
+	for i, header := range headers {
+		headerIdx[header] = i
+	}
+
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ",")
+		l, err := NewLineItem(
+			parts[headerIdx["identity/LineItemId"]],
+			parts[headerIdx["identity/TimeInterval"]],
+			parts[headerIdx["lineItem/AvailabilityZone"]],
+			parts[headerIdx["lineItem/BlendedCost"]],
+			parts[headerIdx["lineItem/BlendedCost"]],
+			parts[headerIdx["lineItem/CurrencyCode"]],
+			parts[headerIdx["lineItem/LegalEntity"]],
+			parts[headerIdx["lineItem/LineItemDescription"]],
+			parts[headerIdx["lineItem/LineItemType"]],
+			parts[headerIdx["lineItem/NormalizationFactor"]],
+			parts[headerIdx["lineItem/Operation"]],
+			parts[headerIdx["lineItem/ProductCode"]],
+			parts[headerIdx["lineItem/ResourceId"]],
+			parts[headerIdx["lineItem/TaxType"]],
+			parts[headerIdx["lineItem/UnblendedCost"]],
+			parts[headerIdx["lineItem/UnblendedRate"]],
+			parts[headerIdx["lineItem/UsageAccountId"]],
+			parts[headerIdx["lineItem/UsageAmount"]],
+			parts[headerIdx["lineItem/UsageStartDate"]],
+			parts[headerIdx["lineItem/UsageEndDate"]],
+			parts[headerIdx["lineItem/UsageType"]],
+		)
+		if err != nil {
+			return nil, err
+		}
+		l.Bill, err = NewBill(
+			parts[headerIdx["bill/Entity"]],
+			parts[headerIdx["bill/BillType"]],
+			parts[headerIdx["bill/InvoiceId"]],
+			parts[headerIdx["bill/PayerAccountId"]],
+			parts[headerIdx["bill/BillingPeriodStartDate"]],
+			parts[headerIdx["bill/BillingPeriodEndDate"]],
+		)
+		if err != nil {
+			return nil, err
+		}
+		r.AddLineItem(l)
+	}
+
+	err = fh.Close()
+	if gzerr := gz.Close(); err == nil {
+		err = gzerr
+	}
+
+	return r, err
 }
 
-type Identity struct {
-	LineItemIDs map[time.Time][]*LineItemID // map of start timestamps to a slice of LineItemIDs
-	TimePts     []time.Time                 // sorted order of start timestamps with identity
-}
-
-func NewIdentity() *Identity {
-	return &Identity{LineItemIDs: make(map[time.Time][]*LineItemID)}
-}
-
-func (i *Identity) AddLineItemID(l *LineItemID) {
-	lids, exists := i.LineItemIDs[l.Start]
+func (r *Report) AddLineItem(l *LineItem) {
+	lids, exists := r.LineItems[l.Start]
 	if exists {
 		for _, lid := range lids {
 			if lid.UID == l.UID {
@@ -68,34 +109,53 @@ func (i *Identity) AddLineItemID(l *LineItemID) {
 		return
 	}
 
-	i.LineItemIDs[l.Start] = []*LineItemID{l}
+	r.LineItems[l.Start] = []*LineItem{l}
 
 	// linearly scan to find where to put timepoint in sorted order
 	// most cases should be at the end
-	for j := len(i.TimePts) - 1; j >= 0; j-- {
-		if l.Start.After(i.TimePts[j]) {
+	for j := len(r.TimePts) - 1; j >= 0; j-- {
+		if l.Start.After(r.TimePts[j]) {
 			// timestamp is after latest timestamp
-			if j == len(i.TimePts)-1 {
-				i.TimePts = append(i.TimePts, l.Start)
+			if j == len(r.TimePts)-1 {
+				r.TimePts = append(r.TimePts, l.Start)
 				return
 			}
 
 			// timestamp is somewhere in between
-			i.TimePts = append(i.TimePts, time.Time{})
-			copy(i.TimePts[j+2:], i.TimePts[j+1:])
-			i.TimePts[j+1] = l.Start
+			r.TimePts = append(r.TimePts, time.Time{})
+			copy(r.TimePts[j+2:], r.TimePts[j+1:])
+			r.TimePts[j+1] = l.Start
 			return
 		}
 	}
 
 	// timepoint is earlier than all timepoints
-	i.TimePts = append(i.TimePts, time.Time{})
-	copy(i.TimePts[1:], i.TimePts[:])
-	i.TimePts[0] = l.Start
+	r.TimePts = append(r.TimePts, time.Time{})
+	copy(r.TimePts[1:], r.TimePts[:])
+	r.TimePts[0] = l.Start
 	return
 }
 
+func (r Report) FilterByTime(s, e time.Time) []*LineItem {
+	startIdx := 0
+	endIdx := len(r.TimePts)
+	var startFound, endFound bool
+	for i, t := range r.TimePts {
+		if startFound && endFound {
+			break
+		}
+		if s.After(t) {
+			endIdx = i
+			break
+		}
+	}
+}
+
 type LineItem struct {
+	UID   uint64
+	Start time.Time
+	End   time.Time
+
 	AvailabilityZone    string
 	BlendedCost         float64
 	BlendedRate         float64
@@ -115,14 +175,30 @@ type LineItem struct {
 	UsageEndDate        time.Time
 	UsageStartDate      time.Time
 	UsageType           string
+
+	Bill *Bill
 }
 
-func NewLineItem(az, blendedCost, blendedRate, currencyCode, legalEntity, lineItemDescription, lineItemType,
-	normalizationFactor, operation, productCode, resourceID, taxType, unblendedCost, unblendedRate,
-	usageAccountID, usageAmount, start, end, usageType string) (*LineItem, error) {
+func NewLineItem(id, timeInterval, az, blendedCost, blendedRate, currencyCode, legalEntity,
+	lineItemDescription, lineItemType, normalizationFactor, operation, productCode,
+	resourceID, taxType, unblendedCost, unblendedRate, usageAccountID, usageAmount, usageStart,
+	usageEnd, usageType string) (*LineItem, error) {
 	l := new(LineItem)
+	l.UID = xxhash.Sum64String(id)
+	timeIntStr := strings.Split(timeInterval, "/")
+	if len(timeIntStr) != 2 {
+		return nil, fmt.Errorf("Invalid time interval, %s", timeInterval)
+	}
 
 	var err error
+	l.Start, err = time.Parse(timeLayout, timeIntStr[0])
+	if err != nil {
+		return nil, fmt.Errorf("Could not parse start interval, %v", err)
+	}
+	l.End, err = time.Parse(timeLayout, timeIntStr[1])
+	if err != nil {
+		return nil, fmt.Errorf("Coult not parse end interval, %v", err)
+	}
 
 	l.BlendedCost, err = strconv.ParseFloat(blendedCost, 64)
 	if err != nil {
@@ -149,11 +225,11 @@ func NewLineItem(az, blendedCost, blendedRate, currencyCode, legalEntity, lineIt
 		return nil, fmt.Errorf("Could not parse unblendedRate, %v", err)
 	}
 
-	l.UsageStartDate, err = time.Parse(timeLayout, start)
+	l.UsageStartDate, err = time.Parse(timeLayout, usageStart)
 	if err != nil {
 		return nil, fmt.Errorf("Could not parse start interval, %v", err)
 	}
-	l.UsageEndDate, err = time.Parse(timeLayout, end)
+	l.UsageEndDate, err = time.Parse(timeLayout, usageEnd)
 	if err != nil {
 		return nil, fmt.Errorf("Coult not parse end interval, %v", err)
 	}
@@ -206,82 +282,9 @@ func NewBill(entity, billType, invoiceID, payerAccountID, start, end string) (*B
 
 func main() {
 	filename := "/Users/aouyang/Downloads/ao-aws-1.csv.gz"
-	file, err := os.Open(filename)
-
+	report, err := NewReport(filename)
 	if err != nil {
 		logger.Fatal(err)
 	}
-
-	gz, err := gzip.NewReader(file)
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	scanner := bufio.NewScanner(gz)
-
-	scanner.Scan()
-	headers := strings.Split(scanner.Text(), ",")
-	headerIdx := make(map[string]int)
-	for i, header := range headers {
-		headerIdx[header] = i
-	}
-
-	identity := NewIdentity()
-	for scanner.Scan() {
-		parts := strings.Split(scanner.Text(), ",")
-		l, err := NewLineItemID(
-			parts[headerIdx["identity/LineItemId"]],
-			parts[headerIdx["identity/TimeInterval"]],
-		)
-		if err != nil {
-			logger.Fatal(err)
-		}
-		l.Bill, err = NewBill(
-			parts[headerIdx["bill/Entity"]],
-			parts[headerIdx["bill/BillType"]],
-			parts[headerIdx["bill/InvoiceId"]],
-			parts[headerIdx["bill/PayerAccountId"]],
-			parts[headerIdx["bill/BillingPeriodStartDate"]],
-			parts[headerIdx["bill/BillingPeriodEndDate"]],
-		)
-		if err != nil {
-			logger.Fatal(err)
-		}
-		l.LineItem, err = NewLineItem(
-			parts[headerIdx["lineItem/AvailabilityZone"]],
-			parts[headerIdx["lineItem/BlendedCost"]],
-			parts[headerIdx["lineItem/BlendedCost"]],
-			parts[headerIdx["lineItem/CurrencyCode"]],
-			parts[headerIdx["lineItem/LegalEntity"]],
-			parts[headerIdx["lineItem/LineItemDescription"]],
-			parts[headerIdx["lineItem/LineItemType"]],
-			parts[headerIdx["lineItem/NormalizationFactor"]],
-			parts[headerIdx["lineItem/Operation"]],
-			parts[headerIdx["lineItem/ProductCode"]],
-			parts[headerIdx["lineItem/ResourceId"]],
-			parts[headerIdx["lineItem/TaxType"]],
-			parts[headerIdx["lineItem/UnblendedCost"]],
-			parts[headerIdx["lineItem/UnblendedRate"]],
-			parts[headerIdx["lineItem/UsageAccountId"]],
-			parts[headerIdx["lineItem/UsageAmount"]],
-			parts[headerIdx["lineItem/UsageStartDate"]],
-			parts[headerIdx["lineItem/UsageEndDate"]],
-			parts[headerIdx["lineItem/UsageType"]],
-		)
-		if err != nil {
-			logger.Fatal(err)
-		}
-		identity.AddLineItemID(l)
-	}
-
-	logger.Println(identity)
-	// catch any issues while trying to close the file and gzip reader
-	err = file.Close()
-	if gzerr := gz.Close(); err == nil {
-		err = gzerr
-	}
-	if err != nil {
-		logger.Fatal(err)
-	}
-
+	fmt.Println(report)
 }
